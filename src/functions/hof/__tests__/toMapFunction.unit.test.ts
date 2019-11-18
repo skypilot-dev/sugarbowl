708cdc5b21bbbb68a33fa66533296e640eb317c0
William Thorsen <william@skypilot.io>
2019-11-18 15:29:17 -0700

    ft: Can pass params to a map function created from multiple functions
    Modified `toMapFunction()` to allow any function in the chain to accept additional params

--- src/functions/hof/__tests__/toMapFunction.unit.test.ts
+++ src/functions/hof/__tests__/toMapFunction.unit.test.ts
@@ -13,12 +13,33 @@ describe('toMapFunction(:itemFn)', () => {
   it('given 3 functions, return a function that applies all the functions to every item in an array', () => {
     const trim = (aString: string): string => aString.trim();
     const toUpperCase = (aString: string): string => aString.toUpperCase();
-    const omitLetter = (aString: string): string => aString.replace('B', '');
+    const omitLetter = (aString: string, letter: string): string => aString.replace(letter, '');
 
     const mapFn = toMapFunction(trim, toUpperCase, omitLetter);
-
-    const result = mapFn([' abc', 'bcd ']);
+    const result = mapFn([' abc', 'bcd '], 'B');
 
     expect(result).toEqual(['AC', 'CD']);
+  });
+
+  it('given 3 functions that each require params, return a function that accepts all params and applies the combined function with params to every item in an array', () => {
+    const trim = (aString: string): string => aString.trim();
+    const toUpperOrLower = (aString: string, toUpper: boolean): string => toUpper
+      ? aString.toUpperCase()
+      : aString.toLowerCase();
+    const omitLetter = (aString: string, letter: string): string => aString.replace(letter, '');
+
+    const mapFn = toMapFunction(trim, toUpperOrLower, omitLetter);
+
+    /* Define params for the 2nd & 3rd functions. */
+    let toUpper = true;
+    let letterToOmit = 'B';
+    let result = mapFn([' abc', 'bcd '], toUpper, letterToOmit);
+    expect(result).toEqual(['AC', 'CD']);
+
+    /* Check a different set of params for the 2nd & 3rd functions. */
+    toUpper = false;
+    letterToOmit = 'a';
+    result = mapFn(['ABC ', ' DEA'], toUpper, letterToOmit);
+    expect(result).toEqual(['bc', 'de']);
   });
 });
--- src/functions/hof/toMapFunction.ts
+++ src/functions/hof/toMapFunction.ts
@@ -1,22 +1,23 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { extendFunction } from './extendFunction';
-
 
 /* -- Typings -- */
-type ItemFn = (item: any) => any;
+import { extendFunction } from './extendFunction';
 
-type MapFn = (array: any[]) => any[];
+type ItemFn = (...args: any[]) => any;
+
+type MapFn = (array: any[], ...args: any[]) => any[];
 
 
 /* Given a function that acts on a single item, return a function that applies that function to
  * every item in an array. */
 export function doToMapFunction(itemFn: ItemFn): MapFn {
-  return (array) => array.map(itemFn);
+  return (array, ...args: any[]) => array.map((item) => itemFn(item, ...args));
 }
 
 
 export function toMapFunction(...itemFns: ItemFn[]): MapFn {
   const [firstItemFn, ...extendingItemFns] = itemFns;
   const extendedItemFn = extendFunction(firstItemFn, ...extendingItemFns) as ItemFn;
+
   return doToMapFunction(extendedItemFn);
 }
