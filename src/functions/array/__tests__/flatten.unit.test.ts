73e2d9c59270147fbc8a5e99204690869306fe38
William Thorsen <william@thorsen.dev>
2020-04-18 21:32:59 -0600

    refactor: Fix location of flatten test

--- src/functions/array/__tests__/flatten.unit.test.ts
+++ src/functions/array/__tests__/flatten.unit.test.ts
@@ -1,4 +1,4 @@
-import { flatten } from '../../flatten';
+import { flatten } from '../flatten';
 
 describe('flatten()', () => {
   it('given a nested array, should return an array in which all nested elements have been moved to the first level', () => {
