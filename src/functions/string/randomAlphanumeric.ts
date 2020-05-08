48469f12516871f80db06c45c48a91e8f46cf49e
William Thorsen <william@thorsen.dev>
2020-05-08 15:58:27 -0600

    fix: `randomAlphanumeric` may fail to generate a 10-char string
    The `Math.random().toString()` algorithm sometimes results in a shorter string. Switched to an 8-character limit as a workaround.
    Issue Sugar-10

--- src/functions/string/randomAlphanumeric.ts
+++ src/functions/string/randomAlphanumeric.ts
@@ -1,8 +1,8 @@
 import { Integer } from '@skypilot/common-types';
 
-export function randomAlphanumeric(length: Integer = 10): string {
-  if (length < 0 || length > 10) {
-    throw new Error(`Invalid length: ${length}. Length must be from 1 to 10`);
+export function randomAlphanumeric(length: Integer = 8): string {
+  if (length < 0 || length > 8) {
+    throw new Error(`Invalid length: ${length}. Length must be from 0 to 8`);
   }
   return Math.random().toString(36).slice(2, length + 2);
 }
