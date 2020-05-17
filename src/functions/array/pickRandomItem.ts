8ef28b4645c4f1cd2004f855f3be798bbf042b4b
William Thorsen <william@thorsen.dev>
2020-05-16 19:47:18 -0600

    refactor: Improve performance of `pickRandomItem`
    Issue Sugar-11

--- src/functions/array/pickRandomItem.ts
+++ src/functions/array/pickRandomItem.ts
@@ -1,7 +1,10 @@
 import { MaybeReadOnlyArray } from '@skypilot/common-types';
-import { pickRandomItems } from './pickRandomItems';
 
 /* Given an array of items, select one item from it at random and return it. */
 export function pickRandomItem<T>(array: MaybeReadOnlyArray<T>): T {
-  return pickRandomItems<T>(array, 1)[0];
+  if (!array.length) {
+    throw new Error('The array is empty');
+  }
+  return array[Math.floor(Math.random() * array.length)];
 }
+
