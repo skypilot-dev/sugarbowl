0a6514af1e620662f7892994082fed458237554b
William Thorsen <william@thorsen.dev>
2021-01-18 12:52:56 -0800

    util: Can check whether a value is a "plain object"
    Added the `isPlainObject` function, which returns `true` if a given value is an object but not an array, function, primitive, or instance of a class other than `Object`. Not exported yet.

---
+++ src/functions/object/__tests__/isPlainObject.type.test.ts
@@ -0,0 +1,53 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+
+// Uncomment the text line to enable typechecking
+// @ts-nocheck
+
+import { PlainObject } from '../isPlainObject';
+
+function takesPlainObject(_plainObject: PlainObject): void {
+  /* do nothing */
+}
+
+describe('PlainObject', () => {
+  it('should accept any object that is not an array, function, or class instance', () => {
+    type MyObj = {
+      a: string;
+    }
+    const typedObj: MyObj = { a: 'value' };
+
+    takesPlainObject({});
+    takesPlainObject({ a: 1 });
+    takesPlainObject(typedObj);
+    takesPlainObject(new Object());
+  });
+
+  it('should reject any primitive value', () => {
+    takesPlainObject(true);
+    takesPlainObject(0);
+    takesPlainObject('');
+  });
+
+  it('should reject a function', () => {
+    const fn = (): void => void 0;
+    takesPlainObject(fn);
+  });
+
+  it('should reject any array', () => {
+    takesPlainObject([]);
+    takesPlainObject([1]);
+  });
+
+  it('should reject any instance of a class other than Object', () => {
+    class MyClass {
+      constructor(public prop?: unknown) {}
+    }
+
+    takesPlainObject(MyClass);
+    takesPlainObject(new Date());
+    takesPlainObject(new Map());
+    takesPlainObject(new MyClass());
+    takesPlainObject(new Set());
+    takesPlainObject(new WeakMap());
+  });
+});
