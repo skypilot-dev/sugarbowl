27e545de06b3962a21d112c59703b80b092784a6
Camilla Thorsen <57811906+camthorsen@users.noreply.github.com>
2021-03-21 16:56:07 -0700

    docs: Fix explanatory comment

--- src/functions/array/getLastItem.ts
+++ src/functions/array/getLastItem.ts
@@ -7,7 +7,8 @@ export function getLastItem<Item, Default extends Item | undefined>(
   items: ReadonlyArray<Item>, options: GetLastItemOptions<Default>
 ): Item | Default;
 
-// Return the last item in the array, or null if the array is empty
+// Return the last item in the array if the array has items; 
+// otherwise the default value (if provided) or undefined.
 export function getLastItem<Item, Default extends Item>(
   items: ReadonlyArray<Item>, options: GetLastItemOptions<Item> = {}
 ): Item | Default | undefined {
