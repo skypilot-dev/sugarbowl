2b8297e4f99402cb046a70a5efb78bb323e07a0d
William Thorsen <william@thorsen.dev>
2021-04-18 15:00:34 -0700

    feat: Can get typed Object.keys for numbers & strings

--- src/functions/object/getObjectKeys.ts
+++ src/functions/object/getObjectKeys.ts
@@ -7,7 +7,8 @@
 // TODO: Support the other values accepted by `Object.keys`
 
 
-export function getObjectKeys<T extends Array<unknown> | ReadonlyArray<unknown>>(array: T): Array<string>;
+export function getObjectKeys<T extends Array<unknown> | ReadonlyArray<unknown> | string>(array: T): Array<string>;
+export function getObjectKeys<T extends number>(num: T): [];
 export function getObjectKeys<T extends Record<string, unknown>>(obj: T): Array<keyof T>;
 export function getObjectKeys<T>(obj: T): Array<string> | Array<keyof T>  {
   return Object.keys(obj) as (keyof T)[];
