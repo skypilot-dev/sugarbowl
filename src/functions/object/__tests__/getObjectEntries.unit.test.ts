7635770872b9356c7cd9a8517c6ab32477a8be93
William Thorsen <william@thorsen.dev>
2021-04-20 15:58:42 -0700

    feat: Can get typed Object.entries for arrays

--- src/functions/object/__tests__/getObjectEntries.unit.test.ts
+++ src/functions/object/__tests__/getObjectEntries.unit.test.ts
@@ -1,6 +1,6 @@
 import { getObjectEntries } from '../getObjectEntries';
 
-describe('getObjectEntries(obj: Object)', () => {
+describe('getObjectEntries(obj: Object | Array)', () => {
   it("should return the object's entries", () => {
     const obj = { a: 1, b: 2 } as const;
     const expected = [
@@ -13,6 +13,24 @@ describe('getObjectEntries(obj: Object)', () => {
 
   it('given an empty object, should return an empty array', () => {
     const obj = {} as const;
+    const expected = [] as const;
+
+    const objectEntries = getObjectEntries(obj);
+    expect(objectEntries).toStrictEqual(expected);
+  });
+
+  it('given an array, should return its indexes as strings and its items as values', () => {
+    const obj = [1, 2] as const;
+    const expected = [
+      ['0', 1], ['1', 2],
+    ];
+
+    const objectEntries = getObjectEntries(obj);
+    expect(objectEntries).toStrictEqual(expected);
+  });
+
+  it('given an empty array, should return an empty array', () => {
+    const obj = [] as const;
     const expected = [] as const;
 
     const objectEntries = getObjectEntries(obj);
--- src/functions/object/getObjectEntries.ts
+++ src/functions/object/getObjectEntries.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/ban-types */
+
 /*
    Given an object of type `Record<string, any>`, return its entries. This replicates the behaviour of
    `Object.entries` but types the return values correctly.
@@ -5,13 +7,22 @@
 
 // TODO: Support the other values accepted by `Object.entries`
 
-type ObjectValues<O> = O extends Record<string, infer Values>
-  ? Values
-  : never;
+type ObjectValues<O> = O extends ReadonlyArray<infer Values> ? Values
+  : O extends Array<infer Values> ? Values
+    : O extends null ? never
+      : O extends Record<string, infer Values> ? Values
+        : never;
 
-// eslint-disable-next-line @typescript-eslint/ban-types
-export function getObjectEntries<O extends Object>(
-  obj: O
-): Array<[key: keyof O, value: ObjectValues<O>]> {
-  return Object.entries(obj) as [keyof O, ObjectValues<O>][];
+export function getObjectEntries<T extends Array<Values> | ReadonlyArray<Values>, Values>(
+  obj: T
+): Array<[key: string, value: ObjectValues<T>]>;
+export function getObjectEntries<T extends Object>(
+  obj: T
+): Array<[key: keyof T, value: ObjectValues<T>]>;
+
+export function getObjectEntries<T extends Object>(
+  obj: T
+): Array<[key: string, value: ObjectValues<T>]> | Array<[key: keyof T, value: ObjectValues<T>]> {
+  return Array.isArray(obj) ? Object.entries(obj) as Array<[string, ObjectValues<T>]>
+    : Object.entries(obj) as Array<[keyof T, ObjectValues<T>]>;
 }
