0bb5ca20fdb6f9bb1d3836040e8950c12d91d79c
William Thorsen <william@thorsen.dev>
2021-04-25 23:46:28 -0700

    refactor: Export types used with makeDateTimeStamp

--- src/functions/date/index.ts
+++ src/functions/date/index.ts
@@ -1,5 +1,6 @@
 export { isValidDate } from './isValidDate';
 export { makeDateTimeStamp } from './makeDateTimeStamp';
+export type { DateTimeStampOptions, DateTimeStampPresetCode } from './makeDateTimeStamp';
 export { sleep } from './sleep';
 export { slugifyDateTime } from './slugifyDateTime';
 export { toUnixTime } from './toUnixTime';
